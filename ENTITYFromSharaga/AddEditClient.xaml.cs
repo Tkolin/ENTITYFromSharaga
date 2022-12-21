71f8fc66f320f13dedf615081b0572b5cc18f442
Tkolin <Tkolin@GRIGORIY>
2022-12-21 22:29:28 +0700

    Добавьте файлы проекта.

---
+++ ENTITYFromSharaga/AddEditClient.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ENTITYFromSharaga
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditClient.xaml
+    /// </summary>
+    public partial class AddEditClient : Page
+    {
+        pro_9Entities context;
+        Client client;
+        bool add = false;
+        public AddEditClient(pro_9Entities context)
+        {
+            InitializeComponent();
+            DataContext = context;
+
+            this.context = context;
+            add = true;
+
+        }
+        public AddEditClient(pro_9Entities context, Client client)
+        {
+            InitializeComponent();
+            DataContext = context;
+
+            this.context = context;
+            this.client = client;
+            tbox.Text = client.FirstName;
+            //И так далие по всем полям
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {        
+            client.FirstName = tbox.Text;
+            //и тд
+            if (add)
+                context.Client.Add(client);
+            //context.SaveChanges(); раскоментить 
+            NavigationService.GoBack();
+
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ ENTITYFromSharaga/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ENTITYFromSharaga
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        pro_9Entities context;
+        public MainPage()
+        {
+            InitializeComponent();
+            context = new pro_9Entities();
+            grid.ItemsSource = context.Client.ToList();
+            cbox.ItemsSource = context.Gender.ToList();
+        }
+
+        private void tbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterGrid();
+        }
+
+        private void delBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem == null)
+                return;
+            context.Client.Remove((Client)grid.SelectedItem);
+            grid.SelectedItem = context.Client.ToList();
+            //context.SaveChanges();//в этой базе не работает там связи есть надо каскад ставить
+       
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditClient(context));
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem == null)
+                return;
+
+            NavigationService.Navigate(new AddEditClient(context,(Client)grid.SelectedItem));
+        }
+
+        private void cbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterGrid();
+        }
+        private void FilterGrid()
+        {
+            List<Client> list = context.Client.ToList();//тут все клиеты
+            if (cbox.SelectedItem == null && tbox.Text == "")
+                return;
+
+
+            if (cbox.SelectedItem != null)
+            {
+                Gender selectGender = cbox.SelectedItem as Gender;
+                list = list.Where(p => p.Gender == selectGender).ToList();//если гендер есть берём его
+            }
+            if (tbox.Text != "")
+            {
+                list = list.Where(p => p.FirstName.ToLower().Contains(tbox.Text.ToLower())).ToList();//фильтр по имени
+            }
+            grid.ItemsSource = list;
+            
+        }
+
+        private void resetFilter_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text = "";
+            cbox.SelectedItem = null;
+            grid.ItemsSource = context.Client.ToList(); 
+        }
+    }
+}
